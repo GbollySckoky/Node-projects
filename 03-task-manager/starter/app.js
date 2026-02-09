eb1511c0c2a17e67bbe1369a51b253eb452b7aaf
Gbolahan Coker <gbolahancoker99@gmail.com>
2026-02-09 18:02:16 +0100

    first commit

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,12 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+
+// routes
+app.get('/', (req, res) => {
+    res.send('HELLO WORLD')
+})
+
+// app.get()
+const port = 5000
+
+app.listen(port, console.log('HHello world'))
