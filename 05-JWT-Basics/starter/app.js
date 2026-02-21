1f84396c65d02a33b0b729c45157848f16449a2b
Gbolahan Coker <gbolahancoker99@gmail.com>
2026-02-21 17:34:44 +0100

    learnt json web token

--- 05-JWT-Basics/starter/app.js
+++ 05-JWT-Basics/starter/app.js
@@ -1,16 +1,17 @@
 require('dotenv').config();
-require('express-async-errors');
+require('express-async-errors'); // helps us to handle errors in async functions without try-catch blocks in our controllers and middlewares
 
 const express = require('express');
 const app = express();
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
-
+const mainRouter = require('./routes/main');
 // middleware
 app.use(express.static('./public'));
 app.use(express.json());
 
+app.use('/api/v1', mainRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
