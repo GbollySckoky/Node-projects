602729212d72b3f26af3c7038ece94110dd6b7b7
Gbolahan Coker <gbolahancoker99@gmail.com>
2026-03-13 23:21:27 +0100

    update

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -15,7 +15,25 @@ const authenticatedUser = require('./middleware/authentication')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+
 app.use(express.json());
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 // extra packages
 
 // routes
